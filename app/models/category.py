0562aaa97c256149d3d3dab627669d0411e3d228
quanmbl4255142 <162358434+QuanMBL@users.noreply.github.com>
2026-04-04 23:00:18 +0700

    ForeignKey update

--- app/models/category.py
+++ app/models/category.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, DateTime, Text, Boolean
+from sqlalchemy import Column, Integer, String, DateTime, Text, Boolean, ForeignKey
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
 from app.core.database import Base
